942e1a9bcd46eebd1a27218f4cfe817dff704c61
dinnertime.dev <dinnertime.dev@gmail.com>
2026-03-31 19:48:07 +0900

    admin plugin 임시삭제

--- packages/auth/src/plugin.ts
+++ packages/auth/src/plugin.ts
@@ -1,6 +1,5 @@
 import type { BetterAuthPlugin } from "better-auth";
-import { admin } from "better-auth/plugins";
 
 export const authPlugins: BetterAuthPlugin[] = [
-  admin(), //
+  // admin(), //
 ];
